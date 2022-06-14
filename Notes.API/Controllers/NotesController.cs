8196a1f6721e57f7e0e695a1dc55c5041d5a4954
Mohammad Mehdi Shaban Keleshteri <mehdi.shaban.keleshteri@outlook.com>
2022-06-15 00:55:08 +0800

    added NotesController

---
+++ Notes.API/Controllers/NotesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Notes.API.Data;
+
+namespace Notes.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NotesController : Controller
+    {
+        private readonly NotesDbContext notesDbContext;
+
+        public NotesController(NotesDbContext notesDbContext)
+        {
+            this.notesDbContext = notesDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllNotes()
+        {
+            //Get the notes from the database
+            return Ok(await notesDbContext.Notes.ToListAsync());
+        }
+         
+    }
+}
