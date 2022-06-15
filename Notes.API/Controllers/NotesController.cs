ab64619d5504d5c31093c790c491f0c19029a0ff
Mohammad Mehdi Shaban Keleshteri <mehdi.shaban.keleshteri@outlook.com>
2022-06-15 11:21:29 +0800

    Note API endpoint  Done

--- Notes.API/Controllers/NotesController.cs
+++ Notes.API/Controllers/NotesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Notes.API.Data;
+using Notes.API.Models.Entities;
 
 namespace Notes.API.Controllers
 {
@@ -21,6 +22,63 @@ namespace Notes.API.Controllers
             //Get the notes from the database
             return Ok(await notesDbContext.Notes.ToListAsync());
         }
-         
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        [ActionName("GetNoteById")]
+        public async Task<IActionResult> GetNoteById([FromRoute]Guid id)
+        {
+            //await notesDbContext.Notes.FirstOrDefaultAsync(x => x.Id == id)
+            var note = await notesDbContext.Notes.FindAsync(id);
+            if (note == null) 
+            {
+                return NotFound();
+            }
+            return Ok(note);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddNote(Note note)
+        {
+            note.Id = Guid.NewGuid();
+            await notesDbContext.Notes.AddAsync(note);
+            await notesDbContext.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetNoteById),new {id= note.Id},note);
+        }
+
+
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> UpdateNote([FromRoute] Guid id, [FromBody] Note updateNote)
+        {
+            var existingNote = await notesDbContext.Notes.FindAsync(id);
+            if (existingNote == null)
+            {
+                return NotFound();
+            }
+
+            existingNote.Title=updateNote.Title;
+            existingNote.Description=updateNote.Description;
+            existingNote.IsVisible  =updateNote.IsVisible;
+
+            await notesDbContext.SaveChangesAsync();
+
+            return Ok(existingNote);
+        }
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> DeleteNote([FromRoute] Guid id)
+        {
+            var existingNote = await notesDbContext.Notes.FindAsync(id);
+            if (existingNote == null)
+            {
+                return NotFound();
+            }
+              notesDbContext.Notes.Remove(existingNote);
+            await notesDbContext.SaveChangesAsync();
+            return Ok();
+        }
+
     }
 }
